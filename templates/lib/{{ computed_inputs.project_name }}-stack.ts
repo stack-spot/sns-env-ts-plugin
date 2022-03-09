d357c8acd2db39164d081a0d4643013c05b7277c
StackSpot Admin <admin@stackspot.com>
2022-03-09 18:19:02 +0000

    Update public repository

--- templates/lib/{{ computed_inputs.project_name }}-stack.ts
+++ templates/lib/{{ computed_inputs.project_name }}-stack.ts
@@ -8,9 +8,7 @@ class {{ computed_inputs.app_class_name }}Stack extends Stack {
 
     const snsTopic = new SnsAppJsiiComponent(this, '{{ computed_inputs.app_class_name }}Topic', {
       displayName: '{{ inputs.topic_name }}',
-      fifo: {% if inputs.topic_fifo %}true{% else %}false{% endif %},
       topicName: '{{ inputs.topic_name }}',
-      contentBasedDeduplication: false,
     });
   }
 }
